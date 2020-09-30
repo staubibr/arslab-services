b96d1e505474df68171cc7acbd221f6cbefde5d6
Shreya Taneja <shreyataneja@Shreyas-MacBook-Air.local>
2020-09-30 15:48:35 -0400

    get all info function

--- sim.rise.ext/src/main/java/services/ResultsService.java
+++ sim.rise.ext/src/main/java/services/ResultsService.java
@@ -3,6 +3,8 @@ package services;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -19,10 +21,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-
+import org.springframework.mock.web.MockMultipartFile;
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import components.CustomException;
+import components.FilesMap;
+import components.Utilities;
+import models.Parsed;
+import parsers.IParser;
 import util.ConnectionFactory;
 import util.DbUtil;
 
@@ -242,9 +258,39 @@ public class ResultsService {
 	}
 
 	public String getAllInfo(String username, String servicetype, String framework) throws IOException {
-		 Connection connection = ConnectionFactory.getConnection();
+		List<File> simFiles = new ArrayList<>(); 
+		List<MultipartFile> simMultipartFiles = new ArrayList<>(); 
+		Connection connection = ConnectionFactory.getConnection();
+		// InputStream debug = getDebugResults(username,  servicetype,  framework);
+		 InputStream result = getSimResults(username,  servicetype,  framework);
+		 ZipInputStream result_stream = null;
+	
+		try {
 		
-		 ResultSet results = null;
+		  result_stream =  new ZipInputStream(result);
+	
+ 	        ZipEntry zipEntry1 = null;
+ 	       while ((zipEntry1 = result_stream.getNextEntry()) != null) {
+ 	    	  String fileName = zipEntry1.getName();
+ 	    	 System.out.println(zipEntry1.getSize());
+ 	    	    File newFile = new File(fileName);
+	    	    if (zipEntry1.isDirectory()) {
+ 	    	   // 	System.out.println("isDirectory  " + newFile);
+ 	    	        newFile.mkdirs();
+ 	    	    } else {
+ 	    	    	
+ 	    	    	simFiles.add( newFile);
+ 	    	    }
+ 	            
+ 	       }
+ 	      result_stream.close();
+ 	     
+ 	        
+	} catch (IOException ec) {
+        System.out.println("Error while extract the zip: "+ec);
+    }
+	
+		 ResultSet dbresults = null;
 	        try {
 	        	
 	            connection = ConnectionFactory.getConnection();
@@ -258,36 +304,63 @@ public class ResultsService {
 			            pstmt.setString(2, servicetype);
 			            pstmt.setString(3, username);
 
-	            results = pstmt.executeQuery();
+			            dbresults = pstmt.executeQuery();
 	           
-	            while (results.next())
-	            {	System.out.println( results.getInt(1) );
-	            	System.out.println( results.getString(2) );
+	            while (dbresults.next())
+	            {	
 	            	
-	            	InputStream input = new ByteArrayInputStream(results.getBytes(6) );
-	            	  List<ZipEntry> entries = new ArrayList<ZipEntry>();
+	         
 	            	  ZipInputStream zi = null;
 	            	    try {
-	            	        zi = new ZipInputStream(new ByteArrayInputStream(results.getBytes(6)));
+	            	        zi = new ZipInputStream(new ByteArrayInputStream(dbresults.getBytes(6)));
 	            	      
 	            	        ZipEntry zipEntry = null;
 	            	        while ((zipEntry = zi.getNextEntry()) != null) {
-	            	            entries.add(zipEntry);
+	            	        	 String fileName = zipEntry.getName();
+	            	 	    	    File inputFile = new File(  File.separator + fileName);
+
+	            	 	    	    if (zipEntry.isDirectory()) {
+	            	 	    	    	System.out.println("isDirectory  " + inputFile);
+	            	 	    	    
+	            	 	    	    } else {
+	            	 	    	    	
+	            	 	    	    	simFiles.add( inputFile);
+	            	 	    	    }
+	             	        
 	            	        }
-	            	    } finally {
+	            	        zi.close();
+	             	        
+		   	            	  
+	            	    } 
+	            	    catch (IOException e) {
+	            	        System.out.println("Error while extract the zip: "+e);
+	            	    }
+	            	    finally {
 	            	        if (zi != null) {
 	            	            zi.close();
 	            	        }
-	            	    }
-	            	   // return entries;
-	    			System.out.println(input);
-	            	System.out.println( results.getString(3) );
-	            	System.out.println( results.getString(4) );
-	            	System.out.println( results.getString(5)  );
-
-	            	System.out.println( results.getString(7) );
-	            	System.out.println( results.getString(8) );
-	            	System.out.println( results.getString(9) );
+	            	    } 
+	            	    for(int i = 0; i < simFiles.size(); i++) 
+	                	{ 
+//         	 	    	   FileInputStream fileStream = new FileInputStream(simFiles.get(i));
+//        	 	    	    MultipartFile multipartFile = new MockMultipartFile( simFiles.get(i).getName(), fileStream);
+//        	 	    	   
+//        	 	    	    
+//	            	    	simMultipartFiles.add( multipartFile);
+//	                		System.out.println(simMultipartFiles.get(i)); 
+	                		System.out.println(simFiles.get(i)); 
+	                	} 
+	            //	    postFiles( simFiles);
+	            	    
+//	            	System.out.println( dbresults.getInt(1) );
+//		            System.out.println( dbresults.getString(2) );
+//           		System.out.println( dbresults.getString(3) );
+//	            	System.out.println( dbresults.getString(4) );
+//	            	System.out.println( dbresults.getString(5)  );
+//
+//	            	System.out.println( dbresults.getString(7) );
+//	            	System.out.println( dbresults.getString(8) );
+//	            	System.out.println( dbresults.getString(9) );
 	            }
 	            
 	        } catch (SQLException e) {
@@ -301,6 +374,24 @@ public class ResultsService {
 return null;
 }
 
+	private void postFiles(List<MultipartFile> simFiles ) throws IOException {
+		try {
+			FilesMap map = Utilities.Convert(simFiles);
+			IParser parser = new parsers.auto.Auto();
+			Parsed result = parser.Parse(map);
+			
+			map.Close();
+			
+			ResponseEntity<byte[]> ax= Utilities.ByteArrayResponse(result.name, result.toZipByteArray());
+			System.out.println(ax);
+		} 
+		catch (Exception e) {
+		  	throw new CustomException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+	}
+	
+	
+	
 	public String getDebugResultsURL(String username, String servicetype, String framework) {
 		 String URL =  "http://vs1.sce.carleton.ca:8080/cdpp/sim/workspaces" ;
 		URL = URL.concat("/");
